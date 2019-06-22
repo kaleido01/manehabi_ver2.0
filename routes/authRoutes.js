71b7d583c6293de05275458ecd5c3ccba55dcadf
kaleido <kaleido@gmail.com>
2019-06-23 01:42:23 +0900

    change

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,11 @@
 const passport = require("passport");
 const { createToken } = require("../validation/utils");
+const secret = require("../config/keys").secret;
+
+const baseURL =
+	process.env.NODE_ENV === "production"
+		? "https://manehabi02.herokuapp.com"
+		: "http://localhost:3000";
 
 module.exports = app => {
 	app.get(`/auth/twitter`, passport.authenticate("twitter"));
