a0950489b9507d73b4fe049324b409908d5df5d2
kaleido <kaleido@gmail.com>
2019-06-23 01:51:35 +0900

    removeservice-worker

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,6 @@ import ApolloClient from "apollo-boost";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 import App from "./components/App";
-import * as serviceWorker from "./serviceWorker";
 import "semantic-ui-css/semantic.min.css";
 // import withSession from "./components/withSession";
 
@@ -61,8 +60,3 @@ ReactDOM.render(
 	</Router>,
 	document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
